6a52f233ba1cff8e451a7b174072b2c890b9fe22
hgy <1500586026@qq.com>
2024-01-08 11:20:55 +0800

    pytest-html 生成测试报告插件

--- testcase/test_switch_window_handler.py
+++ testcase/test_switch_window_handler.py
@@ -3,6 +3,11 @@ from page.ExternalLinkPage import ExternalLinkPage
 from page.LeftMenuPage import LeftMenuPage
 from page.LoginPage import LoginPage
 class TestWindowHandle:
+    """
+    pytest -s -q testcase/test_switch_window_handler.py --html=report.html
+    --html=report.html 在根目录生成测试报告 名为report.html 文件与样式分离
+    --self-contained-html 生成一个html文件里面包含css样式
+    """
     def test_switch_window_handle(self,driver):
         LoginPage().login(driver,"jay")
         sleep(3)
