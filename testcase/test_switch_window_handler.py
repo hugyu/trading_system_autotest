fba6e1d073b8308bcfcae408efc25b40c2236bc7
hgy <1500586026@qq.com>
2024-02-07 14:17:44 +0800

    allure的一些操作

--- testcase/test_switch_window_handler.py
+++ testcase/test_switch_window_handler.py
@@ -1,3 +1,4 @@
+import allure
 from time import sleep
 from page.ExternalLinkPage import ExternalLinkPage
 from page.LeftMenuPage import LeftMenuPage
@@ -8,10 +9,27 @@ class TestWindowHandle:
     --html=report.html 在根目录生成测试报告 名为report.html 文件与样式分离
     --self-contained-html 生成一个html文件里面包含css样式
     """
+    """
+    pytest --alluredir=UIreport  testcase/test_switch_window_handler.py
+    将测试报告放在UIreport文件夹下
+    allure serve UIreport 查看allure报告
+    """
+    # alllure的描述是窗口句柄
+    @allure.description("窗口句柄")
+    # 颗粒度越来越小
+    @allure.epic("窗口句柄epic")
+    @allure.feature("窗口句柄feature")
+    @allure.story("窗口句柄story")
+    @allure.tag("窗口句柄tag")
     def test_switch_window_handle(self,driver):
-        LoginPage().login(driver,"jay")
-        sleep(3)
-        LeftMenuPage().click_level_one_menu(driver,"外链")
-        sleep(1)
-        title=ExternalLinkPage().goto_imooc(driver)
-        print(title)
+        # 测试的每一步
+        with allure.step("登录"):
+            LoginPage().login(driver,"jay")
+            sleep(3)
+        with allure.step("点击外链"):
+            LeftMenuPage().click_level_one_menu(driver,"外链")
+            sleep(1)
+        with allure.step("断言title"):
+            title=ExternalLinkPage().goto_imooc(driver)
+            print(title)
+            assert title=="慕课网-程序员的梦工厂"
